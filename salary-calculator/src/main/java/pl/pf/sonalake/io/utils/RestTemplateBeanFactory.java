2558ae02c6b4e85011b16e24f41c771e241dcad1
pfutro <piotr.futro@interia.pl>
2019-07-09 19:09:40 +0200

    SalaryCalculator: usunięcie matwych importow, poprawka JavaDoc

--- salary-calculator/src/main/java/pl/pf/sonalake/io/utils/RestTemplateBeanFactory.java
+++ salary-calculator/src/main/java/pl/pf/sonalake/io/utils/RestTemplateBeanFactory.java
@@ -1,21 +1,18 @@
 package pl.pf.sonalake.io.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * klasa fabrykująca beany restTemplate
+ * klasa fabrykująca budowane beany restTemplate
  *
  * @author pfutro
  */
-
 @Configuration
 public class RestTemplateBeanFactory {
 
