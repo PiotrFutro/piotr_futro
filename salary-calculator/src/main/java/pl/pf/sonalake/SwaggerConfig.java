50f4780b7d81026bb0dd02c385fece522be329e6
pfutro <piotr.futro@interia.pl>
2019-07-10 20:22:00 +0200

    SalaryCalculator: javadoc do klasy fabrykującej Docket Swagger.

--- salary-calculator/src/main/java/pl/pf/sonalake/SwaggerConfig.java
+++ salary-calculator/src/main/java/pl/pf/sonalake/SwaggerConfig.java
@@ -8,6 +8,11 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * Klasa fabrykująca bean Docket opisujący usługę
+ *
+ * @author pfutro
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
